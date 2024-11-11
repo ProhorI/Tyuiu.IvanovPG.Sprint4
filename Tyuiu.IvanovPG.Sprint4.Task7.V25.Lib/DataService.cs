34b7b0ba0fe99f4c7c89dce4c252582d1f516c2b
Tyuiu.IvanovPG@outlook.com <ivanovprohor491@gmail.com>
2024-11-12 00:10:20 +0500

    Task7

---
+++ Tyuiu.IvanovPG.Sprint4.Task7.V25.Lib/DataService.cs
@@ -0,0 +1,40 @@
+﻿
+
+
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.IvanovPG.Sprint4.Task7.V25.Lib
+{
+    public class DataService : ISprint4Task7V25
+    {
+        public int Calculate(int n, int m, string value)
+        {
+            int[,] mtrx = new int[n, m];
+
+            int index = 0;
+            for (int i = 0; i < n; i++) 
+            {
+                for (int j = 0; j < m; j++) 
+                {
+                    mtrx[i, j] = int.Parse(value.Substring(i * m + j, 1));
+                }
+                     
+            }
+        
+
+            int count = 0;
+            for (int j = 0; j < n; j++)
+            {
+                for (int i = 0; i < m; j++) 
+                {
+                    if (mtrx[i, j] % 2 != 0) 
+                    {
+                        count += mtrx[i, j];
+                    }
+                } 
+            }
+            return count;
+
+        }
+    }
+}
---
+++ Tyuiu.IvanovPG.Sprint4.Task7.V25/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using Tyuiu.IvanovPG.Sprint4.Task7.V25.Lib;
+
+namespace Tyuiu.IvanovPG.Sprint4.Task7.V25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 4;
+            int colums = 3;
+            int[,] mtrx = new int[rows, colums];
+            string str = "348561792486";
+
+            DataService ds = new DataService();
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                           *");
+            Console.WriteLine("******************************************************************************");
+            int index = 0;
+
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < rows; i++) 
+            {
+                for (int j = 0; j < colums; j++) 
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(rows, colums, str);
+            Console.WriteLine("сумма = " + res);
+            Console.ReadKey();
+        }
+    }
+}
